f1e7643568bb6c6a536b6665301630c1c80ba6e9
Serj-L <svlepniakov@gmail.com>
2021-03-11 13:12:15 +0300

    add adaptive for Work section, fix bugs

--- script/script.js
+++ script/script.js
@@ -1,6 +1,22 @@
+ /* scroll up */
+ var scrbtn = $('.scroll-up-btn');
+
+ $(window).scroll(function() {
+   if ($(window).scrollTop() > 100) {
+     scrbtn.addClass('active');
+   } else {
+     scrbtn.removeClass('active');
+   }
+ });
+
+ scrbtn.on('click', function(e) {
+   e.preventDefault();
+   $('html, body').animate({scrollTop:0}, '100');
+ });
+/* /scroll up */
 
 $(document).ready(function() {
-    /* burger menu Jquery */
+    /* burger menu */
 	$('.header__burger-btn').click(function(event) {
 		$(".header__burger-btn, .header__nav").toggleClass("active")
         $("body").toggleClass("lock")
@@ -9,7 +25,7 @@ $(document).ready(function() {
 		$(".header__burger-btn, .header__nav").removeClass("active")
         $("body").removeClass("lock")
 	});
-    /* /burger menu Jquery */
+    /* /burger menu */
 
     /* testimonials slider */
     $('#slider-btn-1').click(function(event) {
@@ -53,4 +69,55 @@ $(document).ready(function() {
         $(".customers-review-wrapper").not("#review-5").removeClass("customers-review-wrapper--active")
 	});
     /* /testimonials slider */
+
+     /* works slider */
+     $('#works-slider-btn-1').click(function(event) {
+        $('#works-slider-btn-1').addClass("active")
+		$("#work-sample-1").addClass("active")
+        $(".works__slider-btn").not($(this)).removeClass("active")
+        $(".works__sample").not("#work-sample-1").removeClass("active")
+	});
+    $('#works-slider-btn-2').click(function(event) {
+        $('#works-slider-btn-2').addClass("active")
+		$("#work-sample-2").addClass("active")
+        $(".works__slider-btn").not($(this)).removeClass("active")
+        $(".works__sample").not("#work-sample-2").removeClass("active")
+	});
+    $('#works-slider-btn-3').click(function(event) {
+        $('#works-slider-btn-3').addClass("active")
+		$("#work-sample-3").addClass("active")
+        $(".works__slider-btn").not($(this)).removeClass("active")
+        $(".works__sample").not("#work-sample-3").removeClass("active")
+	});
+    $('#works-slider-btn-4').click(function(event) {
+        $('#works-slider-btn-4').addClass("active")
+		$("#work-sample-4").addClass("active")
+        $(".works__slider-btn").not($(this)).removeClass("active")
+        $(".works__sample").not("#work-sample-4").removeClass("active")
+	});
+    $('#works-slider-btn-5').click(function(event) {
+        $('#works-slider-btn-5').addClass("active")
+		$("#work-sample-5").addClass("active")
+        $(".works__slider-btn").not($(this)).removeClass("active")
+        $(".works__sample").not("#work-sample-5").removeClass("active")
+	});
+    $('#works-slider-btn-6').click(function(event) {
+        $('#works-slider-btn-6').addClass("active")
+		$("#work-sample-6").addClass("active")
+        $(".works__slider-btn").not($(this)).removeClass("active")
+        $(".works__sample").not("#work-sample-6").removeClass("active")
+	});
+    $('#works-slider-btn-7').click(function(event) {
+        $('#works-slider-btn-7').addClass("active")
+		$("#work-sample-7").addClass("active")
+        $(".works__slider-btn").not($(this)).removeClass("active")
+        $(".works__sample").not("#work-sample-7").removeClass("active")
+	});
+    $('#works-slider-btn-8').click(function(event) {
+        $('#works-slider-btn-8').addClass("active")
+		$("#work-sample-8").addClass("active")
+        $(".works__slider-btn").not($(this)).removeClass("active")
+        $(".works__sample").not("#work-sample-8").removeClass("active")
+	});
+    /* /works slider */
 });
